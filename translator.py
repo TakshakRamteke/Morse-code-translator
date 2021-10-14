16d362dc52c93e9638bc59458939af895b710727
TakshakRamteke <takshakramteke0708@gmail.com>
2021-10-14 11:26:46 +0530

    Seprated the codes dictionary from the main code

--- translator.py
+++ translator.py
@@ -1,38 +1,12 @@
+import code_dictionary
+
 trial_code = input("Enter your morse code : ")
 
 letters = trial_code.split(' ')  ## Spliting the morse code into individual letters
 
-codes = {
-    'A':'._',
-    'B':'_...',
-    'C':'_._.',
-    'D':'_..',
-    'E':'.',
-    'F':'.._.',
-    'G':'__.',
-    'H':'....',
-    'I':'..',
-    'J':'.___',
-    'k':'_._',
-    'L':'._..',
-    'M':'__',
-    'N':'_.',
-    'O':'___',
-    'P':'.__.',
-    'Q':'__._',
-    'R':'._.',
-    'S':'...',
-    'T':'_',
-    'U':'.._',
-    'V':'..._',
-    'W':'.__',
-    'X':'_.._',
-    'Y':'_.__',
-    'Z':'__..',
-    
-}
+codes = code_dictionary.codes
 
 for letter in letters : 
-    for key, value in codes.items(): 
-        if letter == value :
-            print(key,end=" ")
+    for key, value in codes.items() :
+        if letter == key :
+            print(value,end=" ")
